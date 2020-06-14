36576755f828f5e1cce55811f63ec0355a36dd05
Angela Roscel Almoro <aralmoro@gmail.com>
2020-06-14 18:12:02 +0800

    update about pagE

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -6,8 +6,11 @@ function About() {
     return (
         <div className="about">
             <img className="about-photo" src={aboutPhoto} alt="my photo" />
-            <p>{`I'm Angela Roscel Almoro, a software engineer with 5 years of experience in the industry. I studied BS Computer Science in University of the Philippines Los Baños and graduated Cum Laude.`}</p>
-            <p>My ultimate career goal is to create something using technology that will leave a significant impact in the world and will make it better. In my free time, I love travelling in different countries, watching KDramas, and playing Animal Crossing.</p>
+            <div className="about-description">
+                <p>{`I'm Angela Roscel Almoro, a software engineer with 5 years of experience in the industry. I studied BS Computer Science in University of the Philippines Los Baños and graduated Cum Laude.`}</p>
+                <br />
+                <p>My ultimate career goal is to create something using technology that will leave a significant impact in the world and will make it better. In my free time, I love travelling in different countries, watching KDramas, and fulfilling my duties as Resident Representative in Animal Crossing.</p>
+            </div>
         </div>
     )
 }
