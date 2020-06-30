aa18e164214d657491ab02c24849e333c8350c09
Angela Roscel Almoro <aralmoro@gmail.com>
2020-06-30 22:57:40 +0800

    responsive bg

--- src/components/background/Background.js
+++ src/components/background/Background.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import { p1, p2, p3, p4, p5, p6 } from '../../constants/bg1points';
 import { lerpPoint } from '../../utils/math';
 
-// Hook
 function useWindowSize() {
     const isClient = typeof window === 'object';
 
@@ -34,86 +33,105 @@ function useWindowSize() {
 function Background(props) {
     const canvasRef = useRef(null);
     const size = useWindowSize();
-    const [startTime, setStartTime] = useState(null);
+    let startTime;
 
     useEffect(() => {
-        setStartTime(Date.now());
+        startTime = Date.now();
+        animate();
     }, []);
 
-    const lerpLine = (p, t) => {
+    useEffect(() => {
+        resizeCanvas();
+        draw();
+    }, [size]);
+
+    const resizeCanvas = () => {
+        const canvas = canvasRef.current;
+        canvas.height = canvas.width = Math.min(size.height, size.width);
+    };
+
+    const lerpVertex = (p, t) => {
         return lerpPoint(p.start, p.end, t);
+    };
+
+    const getT = () => {
+        let t = (Date.now() - startTime) / 2500;
+
+        if (t > 1) { t = 1; }
+        t = (--t) * t * t + 1;
+        return t;
+    }
+
+    const animate = () => {
+        draw();
+        const t = getT();
+        if (t < 1) {
+            window.requestAnimationFrame(animate);
+        } else {
+            setTimeout(animate, 100);
+        }
     };
 
     const draw = () => {
         const canvas = canvasRef.current;
-        canvas.width = size.width;
-        canvas.height = size.height;
 
-        if (canvas.getContext) {
-            var ctx = canvas.getContext('2d', { alpha: false });
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+
+            const width = canvas.width;
+            const height = canvas.height;
 
             ctx.fillStyle = 'white';
-            ctx.fillRect(0, 0, size.width, size.height);
-            ctx.fillStyle = '#cffaff';
+            ctx.fillRect(0, 0, width, height);
+            ctx.fillStyle = '#f7b93b';
 
-            let t = (Date.now() - startTime) / 2500;
+            const t = getT();
 
-            if (t > 1) { t = 1; }
-            t = (--t) * t * t + 1;
             ctx.beginPath();
-            const lerpedP1 = lerpLine(p1, t);
-            ctx.moveTo(size.width * lerpedP1.x, size.height * lerpedP1.y);
+            const lerpedP1 = lerpVertex(p1, t);
+            ctx.moveTo(width * lerpedP1.x, height * lerpedP1.y);
 
-            const lerpedP2 = lerpLine(p2, t);
-            ctx.lineTo(size.width * lerpedP2.x, size.height * lerpedP2.y);
+            const lerpedP2 = lerpVertex(p2, t);
+            ctx.lineTo(width * lerpedP2.x, height * lerpedP2.y);
 
-            const lerpedP3 = lerpLine(p3, t);
-            ctx.lineTo(size.width * lerpedP3.x, size.height * lerpedP3.y);
+            const lerpedP3 = lerpVertex(p3, t);
+            ctx.lineTo(width * lerpedP3.x, height * lerpedP3.y);
 
-            const lerpedP4 = lerpLine(p4, t);
-            const lerpedP4C1 = lerpLine(p4.c1, t);
-            const lerpedP4C2 = lerpLine(p4.c2, t);
+            const lerpedP4 = lerpVertex(p4, t);
+            const lerpedP4C1 = lerpVertex(p4.c1, t);
+            const lerpedP4C2 = lerpVertex(p4.c2, t);
             ctx.bezierCurveTo(
-                size.width * (lerpedP4C1.x + (Math.sin(Date.now() / 1400) * 0.05)),
-                size.height * lerpedP4C1.y,
-                size.width * (lerpedP4C2.x + (Math.sin(Date.now() / 2000) * 0.02)),
-                size.height * lerpedP4C2.y,
-                size.width * (lerpedP4.x + (Math.sin(Date.now() / 2000) * 0.02)),
-                size.height * (lerpedP4.y + (Math.cos(Date.now() / 2000) * 0.02)));
+                width * (lerpedP4C1.x + (Math.sin(Date.now() / 1400) * 0.05)),
+                height * lerpedP4C1.y,
+                width * (lerpedP4C2.x + (Math.sin(Date.now() / 2000) * 0.02)),
+                height * lerpedP4C2.y,
+                width * (lerpedP4.x + (Math.sin(Date.now() / 2000) * 0.02)),
+                height * (lerpedP4.y + (Math.cos(Date.now() / 2000) * 0.02)));
 
-            const lerpedP5 = lerpLine(p5, t);
-            const lerpedP5C1 = lerpLine(p5.c1, t);
-            const lerpedP5C2 = lerpLine(p5.c2, t);
+            const lerpedP5 = lerpVertex(p5, t);
+            const lerpedP5C1 = lerpVertex(p5.c1, t);
+            const lerpedP5C2 = lerpVertex(p5.c2, t);
             ctx.bezierCurveTo(
-                size.width * (lerpedP5C1.x + (Math.sin(Date.now() / 2000) * 0.02)),
-                size.height * lerpedP5C1.y,
-                size.width * (lerpedP5C2.x + (Math.sin(Date.now() / 1200) * 0.04)),
-                size.height * lerpedP5C2.y,
-                size.width * lerpedP5.x,
-                size.height * lerpedP5.y);
+                width * (lerpedP5C1.x + (Math.sin(Date.now() / 2000) * 0.02)),
+                height * lerpedP5C1.y,
+                width * (lerpedP5C2.x + (Math.sin(Date.now() / 1200) * 0.04)),
+                height * lerpedP5C2.y,
+                width * lerpedP5.x,
+                height * lerpedP5.y);
 
-            const lerpedP6 = lerpLine(p6, t);
-            const lerpedP6C = lerpLine(p6.c, t);
+            const lerpedP6 = lerpVertex(p6, t);
+            const lerpedP6C = lerpVertex(p6.c, t);
             ctx.quadraticCurveTo(
-                size.width * lerpedP6C.x,
-                size.height * lerpedP6C.y,
-                size.width * lerpedP6.x,
-                size.height * lerpedP6.y);
+                width * (lerpedP6C.x + (Math.sin(Date.now() / 1000) * 0.01)),
+                height * (lerpedP6C.y + (Math.cos(Date.now() / 1500) * 0.01)),
+                width * lerpedP6.x,
+                height * lerpedP6.y);
             ctx.fill();
-            if (t < 1) {
-                window.requestAnimationFrame(draw);
-            } else {
-                setTimeout(draw, 100);
-            }
         }
     };
 
-    useEffect(() => {
-        window.requestAnimationFrame(draw);
-    });
-
     return (
-        <canvas ref={canvasRef} width={640} height={425} />
+        <canvas ref={canvasRef} className="canvas" width={100} height={100} />
     )
 }
 export default Background;
--- src/constants/bg1points.js
+++ src/constants/bg1points.js
@@ -8,39 +8,39 @@ const p2 = {
 };
 const p3 = {
     start: { x: 1, y: 0 },
-    end: { x: 0.6, y: 0 }
+    end: { x: 0, y: 0 }
 };
 const p4 = {
     c1: {
         start: { x: 1, y: 0 },
-        end: { x: 0.6, y: 0.25 }
+        end: { x: 0.4, y: 0.25 }
     },
     c2: {
         start: { x: 1, y: 0 },
-        end: { x: 0.7, y: 0.3 }
+        end: { x: 0.4, y: 0.3 }
     },
     start: { x: 1, y: 0 },
-    end: { x: 0.7, y: 0.5 }
+    end: { x: 0.4, y: 0.5 }
 };
 const p5 = {
     c1: {
         start: { x: 1, y: 0 },
-        end: { x: 0.7, y: 0.8 }
+        end: { x: 0.4, y: 0.8 }
     },
     c2: {
         start: { x: 1, y: 0 },
-        end: { x: 0.9, y: 0.65 }
+        end: { x: 0.8, y: 0.65 }
     },
     start: { x: 1, y: 0 },
-    end: { x: 0.9, y: 1 }
+    end: { x: 0.85, y: 0.9 }
 };
 const p6 = {
     c: {
         start: { x: 1, y: 0 },
-        end: { x: 0.95, y: 1.3 }
+        end: { x: 0.86, y: 1 }
     },
     start: { x: 1, y: 0 },
-    end: { x: 1, y: 1 }
+    end: { x: 1, y: 0.9 }
 };
 
 export {
