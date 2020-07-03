21eaa6d6670f9648b832ce3a315cac33bc06a695
Angela Roscel Almoro <aralmoro@gmail.com>
2020-07-04 03:33:14 +0800

    add images to about me

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import aboutPhoto from '../../assets/images/angela.png';
+import acIsland from '../../assets/images/ac-island.png';
+import koreaPhoto from '../../assets/images/korea.png';
+import cambodiaPhoto from '../../assets/images/cambodia.png';
+import thailandPhoto from '../../assets/images/thailand.png';
+import taiwanPhoto from '../../assets/images/taiwan.png';
 import acLogo from '../../assets/images/animal-crossing-logo.jpg';
 import './About.scss';
 
@@ -13,25 +18,30 @@ function About() {
                 <div className="about-description">
                     <p>{`I'm Angela Roscel Almoro, a software engineer with 5 years of experience in the industry. I studied BS Computer Science in University of the Philippines Los Baños and graduated Cum Laude.`}</p>
                     <br />
-                    <p>My ultimate career goal is to create something using technology that will leave a significant impact in the world and will make it better. In my free time, I love travelling in different countries, watching KDramas, and fulfilling my duties as Resident Representative in Animal Crossing.</p>
+                    <p>My ultimate career goal is to create something using technology that will leave a significant impact in the world and will make it better. In my free time, I love watching KDramas, fulfilling my duties as Resident Representative in Animal Crossing and travelling in different countries.</p>
+                    <br />
+                    <p>Here are some of my favorite travel photos :) </p>
                 </div>
             </div>
             <div className="about-additional-content">
                 <div className="about-overlay first">
-                    <img className="about-photo" src={acLogo} alt="my photo" />
+                    <img className="about-photo" src={taiwanPhoto} alt="my photo" />
+                    <span className="about-photo-description">Zhongshe Flower Market, Taichung, Taiwan</span>
                 </div>
 
                 <div className="about-overlay second">
-                    <img className="about-photo" src={acLogo} alt="my photo" />
+                    <img className="about-photo" src={koreaPhoto} alt="my photo" />
+                    <span className="about-photo-description">Bukchon Hanok Village, Seoul, South Korea</span>
                 </div>
 
                 <div className="about-overlay third">
-                    <img className="about-photo" src={acLogo} alt="my photo" />
+                    <img className="about-photo" src={cambodiaPhoto} alt="my photo" />
+                    <span className="about-photo-description">Angkor Wat, Siem Reap, Cambodia</span>
                 </div>
 
-                <div className="about-overlay fourth">
-                    <img className="about-photo" src={acLogo} alt="my photo" />
-                </div>
+                {/* <div className="about-overlay fourth">
+                    <img className="about-photo" src={thailandPhoto} alt="my photo" />
+                </div> */}
             </div>
         </div>
     )
