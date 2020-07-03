6a01e1d79390c5f7598c2d22094775be12999bb5
Angela Roscel Almoro <aralmoro@gmail.com>
2020-07-04 00:53:18 +0800

    update contact

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -3,20 +3,21 @@ import Link from '../link/Link';
 import IconButton from '../buttons/IconButton';
 import MailIcon from '../../assets/icons/basic_mail.svg';
 import LinkedInIcon from '../../assets/icons/linked_in.svg';
+import SparkleEmoji from '../../assets/images/sparkle-emoji.png';
 import './Contact.scss';
 
 function Contact() {
     return (
         <div className="contact">
             <div className="contact-content">
-                <h3>Let's get in touch! You can reach me through:</h3>
+                <h3>Let's get in touch! <img className="sparkle-emoji" src={SparkleEmoji} /> <br />You can reach me through:</h3>
                 <ul>
                     <li>
-                        <IconButton icon={MailIcon} disabled size={22} />
+                        <IconButton icon={MailIcon} disabled size={28} />
                         <Link title="Email" fontSize={17} href="mailto: aralmoro@gmail.com" dark />
                     </li>
                     <li>
-                        <IconButton icon={LinkedInIcon} disabled size={22} />
+                        <IconButton icon={LinkedInIcon} disabled size={28} />
                         <Link title="LinkedIn" fontSize={17} href="https://www.linkedin.com/in/angela-roscel-almoro-26022287/" dark />
                     </li>
                 </ul>
