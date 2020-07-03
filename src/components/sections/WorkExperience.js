6dc601af600310650b68c59ce0c453ba9db7c503
Angela Roscel Almoro <aralmoro@gmail.com>
2020-07-04 02:03:17 +0800

    add work

--- src/components/sections/WorkExperience.js
+++ src/components/sections/WorkExperience.js
@@ -3,6 +3,8 @@ import React from 'react';
 import samsungLogo from '../../assets/images/samsung-logo.png';
 import stratpointLogo from '../../assets/images/stratpoint-logo.png';
 import calcmenuLogo from '../../assets/images/calcmenu-logo.jpg';
+import grommetLogo from '../../assets/images/grommet-logo.png';
+import ceosLogo from '../../assets/images/ceos-logo.png';
 import './WorkExperience.scss';
 
 function WorkExperience() {
@@ -27,8 +29,9 @@ function WorkExperience() {
 
     const roles = [
         { title: 'Software Engineer', time: 'October 2018 - Present', company: 'Samsung Research and Development Institute Philippines', logo: samsungLogo, tech: ['React', 'VueJS', 'Angular'] },
-        { title: 'Senior Software Engineer', time: 'March - September 2018', company: 'Stratpoint Technologies, Inc.', logo: stratpointLogo, tech: ['Android', 'React', 'ReactNative'] },
-        { title: 'Junior Software Engineer', time: 'August 2015 - March 2018', company: 'Stratpoint Technologies, Inc.', logo: stratpointLogo, tech: ['Android', 'React', 'ReactNative'] },
+        { title: 'Software Engineer', time: 'June 2015 - September 2018', company: 'Stratpoint Technologies, Inc.', logo: stratpointLogo, tech: ['Android', 'React', 'React Native'] },
+        { title: 'Freelance Mobile Developer', time: 'October - December 2018', company: 'Grommet Technologies', logo: grommetLogo, tech: ['React Native'] },
+        { title: 'Freelance Android Developer', time: 'March - December 2017', company: 'CEOS Technosolutions', logo: ceosLogo, tech: ['Android'] },
         { title: 'Programmer Intern', time: 'April - May 2014', company: 'Calcmenu Philippines, Inc', logo: calcmenuLogo, tech: ['NodeJS', 'Socket.io'] }
     ];
 
