a4da3a809e142bb059feecffa5c543bf5d515ec7
Angela Roscel Almoro <aralmoro@gmail.com>
2020-08-09 22:28:52 +0800

    projects content

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react';
 import Slide from 'react-reveal/Slide';
 import Fade from 'react-reveal/Fade';
 import acIsland from '../../assets/images/ac-island.png';
+import portfolio from '../../assets/images/portfolio.png';
+import puzzle from '../../assets/images/puzzle.png';
+import touchDB from '../../assets/images/touchDB.png';
+import eeReviewer from '../../assets/images/ee-reviewer.jpg';
 import './Projects.scss'
 
 function Projects(props) {
@@ -15,11 +19,10 @@ function Projects(props) {
     }, [isActive]);
 
     const projs = [
-        { title: 'aralmoro portfolio', year: '2020', img: '', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus varius ante, sit amet molestie diam commodo eget. Donec dui ipsum, euismod vitae nibh vel, hendrerit lobortis eros. Quisque ut risus ut nulla ultrices feugiat. Nulla eu nunc eget dolor ullamcorper blandit vitae sit amet sem. Integer enim urna, pharetra sed magna id, egestas sodales purus. Proin convallis facilisis porttitor. Nam ante arcu, condimentum eget aliquam eu, hendrerit eu dui. ', tech: ['React', 'Webpack', 'Canvas'] },
-        { title: '15 Puzzle', year: '2018', img: '', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus varius ante, sit amet molestie diam commodo eget. Donec dui ipsum, euismod vitae nibh vel, hendrerit lobortis eros. Quisque ut risus ut nulla ultrices feugiat. Nulla eu nunc eget dolor ullamcorper blandit vitae sit amet sem. Integer enim urna, pharetra sed magna id, egestas sodales purus. Proin convallis facilisis porttitor. Nam ante arcu, condimentum eget aliquam eu, hendrerit eu dui. ', tech: ['VanillaJS'] },
-        { title: 'MovLancer', year: '2018', img: '', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus varius ante, sit amet molestie diam commodo eget. Donec dui ipsum, euismod vitae nibh vel, hendrerit lobortis eros. Quisque ut risus ut nulla ultrices feugiat. Nulla eu nunc eget dolor ullamcorper blandit vitae sit amet sem. Integer enim urna, pharetra sed magna id, egestas sodales purus. Proin convallis facilisis porttitor. Nam ante arcu, condimentum eget aliquam eu, hendrerit eu dui. ', tech: ['Android'] },
-        { title: 'EE Quizzer', year: '2017', img: '', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus varius ante, sit amet molestie diam commodo eget. Donec dui ipsum, euismod vitae nibh vel, hendrerit lobortis eros. Quisque ut risus ut nulla ultrices feugiat. Nulla eu nunc eget dolor ullamcorper blandit vitae sit amet sem. Integer enim urna, pharetra sed magna id, egestas sodales purus. Proin convallis facilisis porttitor. Nam ante arcu, condimentum eget aliquam eu, hendrerit eu dui. ', tech: ['Android'] },
-        { title: 'TouchDB', year: '2015', img: '', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus varius ante, sit amet molestie diam commodo eget. Donec dui ipsum, euismod vitae nibh vel, hendrerit lobortis eros. Quisque ut risus ut nulla ultrices feugiat. Nulla eu nunc eget dolor ullamcorper blandit vitae sit amet sem. Integer enim urna, pharetra sed magna id, egestas sodales purus. Proin convallis facilisis porttitor. Nam ante arcu, condimentum eget aliquam eu, hendrerit eu dui. ', tech: ['Android'] }
+        { title: 'aralmoro portfolio', year: '2020', img: portfolio, description: 'A product of too much time in quarantine due to Covid-19. This website showcases my projects, work experiences and a little personal info for recruiters or anyone curious about me.', tech: ['React', 'Webpack', 'Canvas'] },
+        { title: 'Sliding Puzzle', year: '2018', img: puzzle, description: 'A simple sliding puzzle that consists numbered square titles in random order with 1 missing tile. The goal of the puzzle is to place the tiles in order by sliding the titles using the empty space. This game also has 8 and 15 puzzle mode.', tech: ['VanillaJS'] },
+        { title: 'EE Quizzer', year: '2017', img: eeReviewer, description: 'An android application that contains lessons and quizzes about Electrical Engineering I made for EE friends.', tech: ['Android'] },
+        { title: 'TouchDB', year: '2015', img: touchDB, description: 'The first ever Android application I created way back in college. This app aims to teach students the basics of database by visualizing database and interacting with it using touch gestures.', tech: ['Android'] }
     ];
 
     const renderItem = (item, i) => {
@@ -27,7 +30,7 @@ function Projects(props) {
             <Fade left={i % 2 === 0} right={i % 2 !== 0}>
                 <div className="project-item">
                     <div className="img-overlay">
-                        <img className="project-img" src={acIsland} alt="project screenshot" />
+                        <img className="project-img" src={item.img} alt="project screenshot" />
                     </div>
                     <div className="project-info">
                         <h2>{item.title}</h2>
@@ -49,7 +52,7 @@ function Projects(props) {
             {/* <img src={constructionIcon} attr="https://www.flaticon.com/authors/freepik" />
             Oops! This page is a work in progress */}
             <Slide top>
-                <p className="intro">While most of the projects I've worked on belong to companies I've worked with, <br />I'd like to share a few of the projects I developed in my free time. 👩‍💻 </p>
+                <p className="intro">While most of the projects I've worked on belong to companies I've worked with, <br />I'd like to share a few of the projects I developed in my own time. 👩‍💻 </p>
             </Slide>
             <ul className="project-list">
                 {
